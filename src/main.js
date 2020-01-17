1f47cce45b7d03f488830d23d5f1fb2c83ea9435
ohamax <junji.ohama@toolbit.org>
2020-01-17 21:50:23 +0900

    Publish binary to GitHub by npm run release and support automatically update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
+// Logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -63,3 +71,7 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
